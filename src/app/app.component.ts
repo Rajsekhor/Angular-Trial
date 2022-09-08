b2877d075d1f59f4230df4f785ebf3214bab3522
Rajsekhor Saikia <raj.saikia.rick333@gmail.com>
2022-09-08 16:18:22 +0530

    Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'Calculator';
   count = 1;
 
-  listOfNumbers = [1, 32, 32, 55]
-  listOfNames = ["Ronnie", "Jack", "Wilbur"]
+  // Inputs for calculator
+  num1=0;
+  num2=0;
+  value=0;
+  sym="+";
+  cnt=0;
+
+  calc=() => {
+    this.cnt++;
+    if(this.sym=="+"){
+      this.value=this.num1+this.num2
+    }
+    if(this.sym=="-"){
+      this.value=this.num1-this.num2
+    }
+    if(this.sym=="*"){
+      this.value=this.num1*this.num2
+    }
+    if(this.sym=="/"){
+      this.value=this.num1/this.num2
+    }
+  }
+
+  listOfNumbers = [1, 32, 32, 55];
+  listOfNames = ["Ronnie", "Jack", "Wilbur"];
 
   decreaseCount=() => {
     console.log("Button Clicked")
